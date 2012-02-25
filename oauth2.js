dcb09fc0ef50612572a4df6824bcd21a1c885305
Rad Gruchalski <radek@gruchalski.com>
2012-02-25 23:30:14 +0000

    Large number of architectural and functional changes

--- oauth2.js
+++ oauth2.js
@@ -1,100 +1,223 @@
 var express = require("express")
 	, util = require("util")
 	, qs = require("qs")
-	, hash = require("jshashes");
+	, url = require("url")
+	, OAuth2Server = require("./server/oauth2-server").OAuth2Server
+	, Db = require('mongodb').Db
+	, Connection = require('mongodb').Connection
+	, Server = require('mongodb').Server;
 
 
-function OAuth2Server() {
-	var __scope = this;
-	this.__$handleClientIdLookup = function(client_id) {
-		return true;
-	};
-	this.__$handleAuthCodeStorage = function(client_id, code) {
-		if ( this.__$code_storage == null ) {
-			this.__$code_storage = {};
-		}
-		this.__$code_storage[ code ] = { client_id: client_id, created_at: (new Date()).getTime() };
-	};
-	this.__$handleAuthCodeTimer = function(scope) {
-		if ( __scope.__$code_storage == null ) {
-			__scope.__$code_storage = {};
-		}
-		var runAt = (new Date()).getTime();
-		var toDelete = [];
-		for ( var key in __scope.__$code_storage ) {
-			if ( runAt - __scope.__$code_storage[key].created_at > 60000 ) {
-				toDelete.push(key);
+// MONGO SETUP:
+var MONGOD = {
+	CONNECTION_STRING: "",
+	CONNECTION_PARSED: {},
+	REQUIRES_AUTH: false,
+	USERNAME: "",
+	PASSWORD: "" };
+MONGOD.CONNECTION_STRING = process.env.MONGOLAB_URI || "mongod://127.0.0.1:27017/oauth2-server-test";
+MONGOD.CONNECTION_PARSED = url.parse( MONGOD.CONNECTION_STRING );
+if ( MONGOD.CONNECTION_PARSED.auth ) {
+	MONGOD.REQUIRES_AUTH = true;
+	var authParts = MONGOD.CONNECTION_PARSED.auth.split(":");
+	MONGOD.USERNAME = authParts[0];
+	MONGOD.PASSWORD = authParts[1];
+}
+var mongodClient = new Db(
+	MONGOD.CONNECTION_PARSED.pathname.substr(1, MONGOD.CONNECTION_PARSED.pathname.length )
+	, new Server(
+		MONGOD.CONNECTION_PARSED.hostname+""
+		, parseInt(MONGOD.CONNECTION_PARSED.port)
+		, {} ) );
+mongodClient.open(function(err, p_client) {
+	if ( MONGOD.REQUIRES_AUTH ) {
+		p_client.authenticate( MONGOD.USERNAME, MONGOD.PASSWORD, function( err, collection ) {
+			if ( err != null ) {
+				sys.puts("FATAL: MONGOD - can't authenticate");
+			} else {
+				setupOAuth2Server();
 			}
-		}
-		for ( var i=0; i<toDelete.length; i++ ) {
-			delete __scope.__$code_storage[ key ];
-		}
-		
-		util.puts("Clear timer " + JSON.stringify( __scope.__$code_storage ) );
-	};
-	
-	this.__$expireAuthCodeTimer = setInterval( __scope.__$handleAuthCodeTimer, 5000 );
-};
-OAuth2Server.prototype.processAuth = function(response_type, client_id, scope, state) {
-	var resp = {};
-	if ( response_type == null ) {
-		resp.error = "invalid_request";
-		resp.error_description = "Parameter response_type not given.";
-		if ( state != null ) {
-			resp.state = state;
-		}
-		return resp;
-	}
-	
-	if ( client_id == null ) {
-		resp.error = "invalid_request";
-		resp.error_description = "Parameter client_id not given.";
-		if ( state != null ) {
-			resp.state = state;
-		}
-		return resp;
-	}
-	
-	if ( response_type !== "code" ) {
-		resp.error = "invalid_request";
-		resp.error_description = "Parameter response_type has a wrong value.";
-		if ( state != null ) {
-			resp.state = state;
-		}
-		return resp;
-	}
-	
-	if ( this.__$handleClientIdLookup( client_id ) ) {
-		resp.code = this.generateAuthCode( client_id );
-		this.__$handleAuthCodeStorage( client_id, resp.code );
-		if ( state != null ) {
-			resp.state = state;
-		}
-		return resp;
+		} );
 	} else {
-		resp.error = "unauthorized_client";
-		return resp;
+		setupOAuth2Server();
+	}
+});
+
+var oauth2 = null;
+function setupOAuth2Server() {
+	util.puts("Setting up OAuth2, connected to mongod, reading scopes for this app");
+	mongodClient.collection("scopes", function(err, collection) {
+		collection.find({}, function(err, cursor) {
+			cursor.toArray( function(err, arr) {
+				
+				util.puts( " -> OAuth2 server initialized with scopes: " + JSON.stringify( arr ) );
+				
+				oauth2 = new OAuth2Server({ scopes: arr });
+				oauth2.accountLogin = function( username, password, callback ) {
+					mongodClient.collection("users", function(err, collection) {
+						collection.find( { username: username }, function(err, cursor) {
+							cursor.toArray( function( err, arr ) {
+								if ( arr.length == 0 ) {
+									callback( null );
+								} else {
+									if ( arr[0].password == password ) {
+										callback( arr[0] );
+									} else {
+										callback( null );
+									}
+								}
+							} );
+						});
+					});
+				};
+				oauth2.accountAllowedScopes = function( account_id, client_id ) {
+				};
+				oauth2.clientIdLookup = function(client_id, callback) {
+					mongodClient.collection("apps", function(err, collection) {
+						collection.find( { client_id: client_id }, function(err, cursor) {
+							cursor.toArray( function( err, arr ) {
+								if ( arr.length == 0 ) {
+									callback( null );
+								} else {
+									callback( arr[0] );
+								}
+							} );
+						} );
+					} );
+				};
+				
+			});
+		});
+	});
+}
+
+var app = express.createServer();
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+
+app.get("/", function(req,res) {
+	res.send("<a href='http://localhost:3000/oauth2/auth?response_type=code&client_id=O0-rdIQGiKqihdwE9-DqudbWY&redirect_uri=" + encodeURIComponent("http://testoauth2:2800/oauth2callback") + "&scope=read edit new account'>do the test</a>");
+});
+
+app.get("/oauth2/logout", function(req,res) {
+	res.clearCookie("logged_in");
+	res.redirect("/");
+});
+
+app.get("/oauth2/login", function(req,res) {
+	
+	var sessionCode = req.param("ses", null);
+	if ( sessionCode == null ) {
+		res.writeHead(401, "Session expired");
+		res.end();
+		return;
 	}
 	
-}
-OAuth2Server.prototype.generateAuthCode = function( client_id ) {
-	var code = new hash.SHA1().b64( (new Date()).toString() + Math.random() + client_id + this.randomString(8) );
-	return code;
-};
-OAuth2Server.prototype.randomString = function(length) {
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-	var randomstring = '';
-	for (var i=0; i<length; i++) {
-		var rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum,rnum+1);
+	if ( oauth2.isLoginSessionCodeValid( oauth2.fixString( sessionCode ) ) ) {
+		var error = req.cookies.error;
+		var html = "<form action='/oauth2/do-login' method='post'>";
+		if ( error != null ) {
+			html += "There was an error: " + error;
+			res.clearCookie("error");
+		}
+		html += "<input type='hidden' name='ses' value='" + sessionCode + "' /><br/>";
+		html += "Username: <input type='text' name='username' /><br/>";
+		html += "Password: <input type='password' name='password' /><br/>";
+		html += "<input type='submit' value='Login' />";
+		html += "</form>";
+		res.send(html);
+	} else {
+		res.writeHead(401, "Session expired " + oauth2.fixString( sessionCode ) );
+		res.end();
 	}
-	return randomstring;
-}
+});
 
-
-var oauth2 = new OAuth2Server();
-var app = express.createServer();
-app.use(express.bodyParser());
+app.post("/oauth2/do-login", function(req,res) {
+	var sessionCode = req.param("ses", null);
+	if ( sessionCode == null ) {
+		res.writeHead(401, "Session expired");
+		res.end();
+		return;
+	}
+	if ( oauth2.isLoginSessionCodeValid( oauth2.fixString( sessionCode ) ) ) {
+		oauth2.accountLogin( req.param("username", null), req.param("password", null), function( account ) {
+			if ( account != null ) {
+				res.cookie("logged_in", account.id);
+				res.redirect( "/oauth2/scopes?ses=" + sessionCode );
+			} else {
+				res.cookie( "error", "Could not log you in." );
+				res.redirect("/oauth2/login?ses=" + sessionCode);
+			}
+		} );
+	} else {
+		res.writeHead(401, "Session expired " + oauth2.fixString( sessionCode ));
+		res.end();
+	}
+});
+app.get("/oauth2/scopes", function(req,res) {
+	var sessionCode = req.param("ses", null);
+	if ( sessionCode == null ) {
+		res.writeHead(401, "Session expired");
+		res.end();
+		return;
+	}
+	
+	sessionCode = oauth2.fixString( sessionCode );
+	
+	if ( oauth2.isLoginSessionCodeValid( sessionCode ) ) {
+		var data = oauth2.getOauth2InputBySessionLoginCode( sessionCode );
+		
+		oauth2.clientIdLookup( data.client_id, function(clientApp) {
+			if ( clientApp == null ) {
+				res.writeHead(400, "Client ID invalid");
+				res.end();
+			} else {
+				
+				var html = "<strong>" + clientApp.name + "</strong> is requesting for the following permissions:";
+				html += "<ul>";
+				
+				var requestedScopes = data.scope.split(" ");
+				var scopes = [];
+				for ( var i=0; i<requestedScopes.length; i++ ) {
+					var aScopeName = oauth2.getScopeName( requestedScopes[i] );
+					if ( aScopeName == null ) {
+						// TODO: if redirect_uri specified, send back
+						res.writeHead(400, "invalid_scope: " + requestedScopes[i]);
+						res.end();
+						return;
+					} else {
+						html += "<li>" + aScopeName + "</li>";
+					}
+				}
+				
+				html += "</ul>";
+				html += "<form action='/oauth2/do-scopes' method='post'>";
+				html += "<input type='hidden' name='access_status' value='allow' />";
+				html += "<input type='button' value='Allow access' />";
+				html += "</form>";
+				
+				html += "<form action='/oauth2/do-scopes' method='post'>";
+				html += "<input type='hidden' name='access_status' value='deny' />";
+				html += "<input type='submit' value='No, thanks' />";
+				html += "</form>";
+				res.send( html );
+			}
+		});
+		
+	} else {
+		res.writeHead(401, "Session expired");
+		res.end();
+	}
+	
+});
+app.get("/oauth2/do-scopes", function(req,res) {
+	var sessionCode = req.param("ses", null);
+	if ( sessionCode == null ) {
+		res.writeHead(401, "Session expired");
+		res.end();
+		return;
+	}
+});
 
 app.get("/oauth2/auth", function(req,res) {
 	
@@ -104,6 +227,29 @@ app.get("/oauth2/auth", function(req,res) {
 	var scope = req.param("scope", null);
 	var state = req.param("state", null);
 	
+	if ( client_id != null ) client_id = oauth2.fixString( client_id );
+	
+	oauth2.clientIdLookup( client_id, function(clientApp) {
+		if ( clientApp == null ) {
+			res.writeHead(400, "Client ID invalid");
+			res.end();
+		} else {
+			
+			if ( req.cookies.logged_in != null ) {
+				if ( scope == null ) {
+					scope = [];
+				}
+				
+			} else {
+				var loginSessionCode = oauth2.generateLoginSessionCode(client_id);
+				oauth2.doLoginSessionCodeStorage( client_id, loginSessionCode, redirect_uri, response_type, scope, state );
+				res.redirect("/oauth2/login?ses=" + loginSessionCode);
+			}
+			
+		}
+	} );
+	
+	/*
 	var authResponse = oauth2.processAuth(
 		response_type
 		, client_id
@@ -116,7 +262,7 @@ app.get("/oauth2/auth", function(req,res) {
 	} else {
 		res.send( JSON.stringify( authResponse ) );
 	}
-	
+	*/
 });
 
 app.listen(3000);
---
+++ server/oauth2-server.js
@@ -0,0 +1,168 @@
+var util = require("util")
+	, hash = require("jshashes");
+
+function OAuth2Server( settings ) {
+	
+	var config = settings || {};
+	var __scope = this;
+	
+	this.scopes = config.scopes || [];
+	this.authCodeTimeout = config.authCodeTimeout || 60000;
+	this.loginSessionCodeTimeout = config.loginSessionCodeTimeout || 60000;
+	this.requireAllScopes = true;
+	
+	this.__$handleAuthCodeTimer = function(scope) {
+		if ( __scope.__$code_storage == null ) {
+			__scope.__$code_storage = {};
+		}
+		var runAt = (new Date()).getTime();
+		var toDelete = [];
+		for ( var key in __scope.__$code_storage ) {
+			if ( runAt - __scope.__$code_storage[key].created_at > __scope.authCodeTimeout ) {
+				toDelete.push(key);
+			}
+		}
+		for ( var i=0; i<toDelete.length; i++ ) {
+			delete __scope.__$code_storage[ key ];
+		}
+	};
+	this.__$handleSessionLoginCodeTimer = function(scope) {
+		if ( __scope.__$session_code_storage == null ) {
+			__scope.__$session_code_storage = {};
+		}
+		var runAt = (new Date()).getTime();
+		var toDelete = [];
+		for ( var key in __scope.__$session_code_storage ) {
+			if ( runAt - __scope.__$session_code_storage[key].created_at > __scope.loginSessionCodeTimeout ) {
+				toDelete.push(key);
+			}
+		}
+		for ( var i=0; i<toDelete.length; i++ ) {
+			delete __scope.__$session_code_storage[ key ];
+		}
+	};
+	
+	this.__$expireAuthCodeTimer = setInterval( __scope.__$handleAuthCodeTimer, 5000 );
+	this.__$expireSessionLoginCodeTimer = setInterval( __scope.__$handleSessionLoginCodeTimer, 5000 );
+};
+
+// TO OVERRIDE
+OAuth2Server.prototype.accountLogin = function( client_id ) { return null };
+OAuth2Server.prototype.accountAllowedScopes = function( client_id ) { return null };
+OAuth2Server.prototype.clientIdExistsLookup = function( client_id ) { return null };
+
+// INTERNALS:
+
+OAuth2Server.prototype.getScopeName = function(scope) {
+	for ( var i=0; i<this.scopes.length; i++ ) {
+		if ( this.scopes[i].key == scope ) {
+			return this.scopes[i].name;
+		}
+	}
+	return null;
+};
+
+OAuth2Server.prototype.doAuthCodeStorage = function(client_id, code) {
+	if ( this.__$code_storage == null ) {
+		this.__$code_storage = {};
+	}
+	this.__$code_storage[ code ] = { client_id: client_id, created_at: (new Date()).getTime() };
+};
+
+OAuth2Server.prototype.doLoginSessionCodeStorage = function( client_id, code, redirect_uri, response_type, scope, state ) {
+	if ( this.__$session_code_storage == null ) {
+		this.__$session_code_storage = {};
+	}
+	this.__$session_code_storage[ code ] = {
+		client_id: client_id
+		, redirect_uri: redirect_uri
+		, response_type: response_type
+		, scope: scope
+		, state: state
+		, created_at: (new Date()).getTime() };
+};
+
+OAuth2Server.prototype.isLoginSessionCodeValid = function( code ) {
+	if ( this.__$session_code_storage == null ) {
+		this.__$session_code_storage = {};
+	}
+	return ( this.__$session_code_storage[ code ] != null );
+};
+
+OAuth2Server.prototype.getOauth2InputBySessionLoginCode = function( code ) {
+	return this.__$session_code_storage[ code ];
+};
+
+OAuth2Server.prototype.processLookup = function(response_type, client_id, scope, state) {
+	var resp = {};
+	if ( response_type == null ) {
+		resp.error = "invalid_request";
+		resp.error_description = "Parameter response_type not given.";
+		if ( state != null ) {
+			resp.state = state;
+		}
+		return resp;
+	}
+	
+	if ( client_id == null ) {
+		resp.error = "invalid_request";
+		resp.error_description = "Parameter client_id not given.";
+		if ( state != null ) {
+			resp.state = state;
+		}
+		return resp;
+	}
+	
+	if ( response_type !== "code" ) {
+		resp.error = "invalid_request";
+		resp.error_description = "Parameter response_type has a wrong value.";
+		if ( state != null ) {
+			resp.state = state;
+		}
+		return resp;
+	}
+	
+	if ( this.clientIdExistsLookup( client_id ) != null ) {
+		
+		resp.scope = scope;
+		resp.state = state;
+		// 
+		/*
+		resp.code = this.generateAuthCode( client_id );
+		this.doAuthCodeStorage( client_id, resp.code );
+		if ( state != null ) {
+			resp.state = state;
+		}
+		return resp;
+		*/
+		
+		
+	} else {
+		resp.error = "unauthorized_client";
+		return resp;
+	}
+	
+}
+OAuth2Server.prototype.generateLoginSessionCode = function( client_id ) {
+	var code = new hash.SHA1().b64( (new Date()).toString() + Math.random() + client_id + this.randomString(50) );
+	return code;
+};
+OAuth2Server.prototype.generateAuthCode = function( client_id ) {
+	var code = new hash.SHA1().b64( (new Date()).toString() + Math.random() + client_id + this.randomString(8) );
+	return code;
+};
+OAuth2Server.prototype.randomString = function(length) {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var randomstring = '';
+	for (var i=0; i<length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	return randomstring;
+};
+OAuth2Server.prototype.fixString = function(str) {
+	str = str.replace(/\s/,"+");
+	return str;
+};
+
+exports.OAuth2Server = OAuth2Server;
